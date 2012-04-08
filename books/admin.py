f3a51bb02f2c4579139edae6ab0190b55453a03c
Rag Sagar.V <ragsagar@gmail.com>
2012-04-08 16:29:47 +0530

    added Branch table, added some extra fields in PolicyIssue table, added code to calculate agent_commission and auto populate it when the object is saved

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from books.models import Agent, PolicyIssue
+from books.models import Agent, PolicyIssue, Branch
 
 
 
@@ -12,11 +12,18 @@ class PolicyIssueAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         obj.added_by = request.user
+        obj.agent_percentage = obj.agent.percentage
+        obj.agent_commission = (obj.premium_amount * obj.agent_percentage)-\
+        obj.customer_discount
         obj.save()
         
 class AgentAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Agent._meta.fields]
+    
+class BranchAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Branch._meta.fields]
 
 
 admin.site.register(PolicyIssue, PolicyIssueAdmin)
 admin.site.register(Agent, AgentAdmin)
+admin.site.register(Branch, BranchAdmin)
--- books/models.py
+++ books/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 
 
 
+class Branch(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Agent(models.Model):
     name = models.CharField(max_length=50)
     percentage = models.DecimalField(max_digits=5,decimal_places=2)
@@ -14,19 +22,23 @@ class Agent(models.Model):
 class PolicyIssue(models.Model):
     BUSINESS_TYPE_CHOICES = (("New", "New"), ("Rollover", "Rollover"),
     ("Renewal", "Renewal"))
+    # following two fields will be helpful in future
     timestamp = models.DateTimeField(default=datetime.now, editable=False)
     added_by = models.ForeignKey(User, editable=False)
     agent = models.ForeignKey(Agent)
+    branch = models.ForeignKey(Branch)
     # columns copied from excel statement
     policy_no = models.CharField(max_length=30) 
     transaction_id = models.CharField(max_length=50, editable=False, blank=True)
     endorsement = models.CharField(max_length=20, editable=False, blank=True)
-    policy_date = models.DateTimeField(default=datetime.now)
+    policy_date = models.DateTimeField(default=datetime.now,
+    blank=True)
     branch_code = models.CharField(max_length=10, editable=False, blank=True)
     product_code = models.CharField(max_length=10, editable=False, blank=True)
     product_name = models.CharField(max_length=100, editable=False, blank=True)
     group_name = models.CharField(max_length=50, editable=False, blank=True)
-    insured_name = models.CharField(max_length=50, blank=True)
+    customer_name = models.CharField(max_length=50, blank=True)
+    mobile_no = models.IntegerField(max_length=11, blank=True)
     premium_amount = models.DecimalField(max_digits=12, decimal_places=4,
             blank=True, null=True)
     agent_code = models.CharField(max_length=12, blank=True)
@@ -34,8 +46,8 @@ class PolicyIssue(models.Model):
             blank=True)
     business_type = models.CharField(choices=BUSINESS_TYPE_CHOICES,
             max_length=10, blank=True)
-    discount = models.DecimalField(max_digits=9, decimal_places=4, blank=True,
-            null=True)
+    discount = models.DecimalField(max_digits=9, decimal_places=4,
+            editable=False, blank=True, null=True)
     irda_percentage = models.DecimalField(verbose_name="IRDA (%)",max_digits=12,
             decimal_places=4, blank=True, null=True)
     bas_percentage = models.DecimalField(verbose_name="BAS (%)", max_digits=12,
@@ -60,6 +72,15 @@ class PolicyIssue(models.Model):
     model = models.CharField(max_length=50, editable=False, blank=True)
     rsd = models.CharField(max_length=20, editable=False, blank=True)
     process_date = models.CharField(max_length=20, editable=False, blank=True)
+    # fields required that are not with reliance statement
+    customer_discount = models.DecimalField(default=0, max_digits=10,
+            decimal_places=4)
+    # following fields will be automatically populated when object is saved
+    agent_percentage = models.DecimalField(max_digits=5, decimal_places=2,
+            editable=False, blank=True, null=True)
+    agent_commission = models.DecimalField(max_digits=15, decimal_places=4,
+            editable=False, blank=True, null=True)
+
 
     def __unicode__(self):
         return self.policy_no
